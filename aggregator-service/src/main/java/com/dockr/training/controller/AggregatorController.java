fffff765ee9b7c35767a521667a41f4969a37039
PHX-DC\deepajai <d.jain@dhl.com>
2020-09-07 15:30:31 +0530

    testing

--- aggregator-service/src/main/java/com/dockr/training/controller/AggregatorController.java
+++ aggregator-service/src/main/java/com/dockr/training/controller/AggregatorController.java
@@ -27,11 +27,11 @@ public class AggregatorController {
 
 		LOGGER.info("Received Request for user id {}. ", id);
 
-		User user = userServiceProxy.retrieveUser(id);
+		//User user = userServiceProxy.retrieveUser(id);
 		Orders orders = ordersServiceProxy.getOrders(id);
 
 		Orderdetails orderDetails = new Orderdetails();
-		orderDetails.setUserDetails(user);
+		//orderDetails.setUserDetails(user);
 		orderDetails.setOrders(orders);
 		return orderDetails;
 
--- aggregator-service/src/main/java/com/dockr/training/controller/UserServiceProxy.java
+++ aggregator-service/src/main/java/com/dockr/training/controller/UserServiceProxy.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.dockr.training.model.User;
 
-@FeignClient(name = "user-service", url = "${USER_SERVICE_URI:http://localhost:8081}")
+//@FeignClient(name = "user-service", url = "${USER_SERVICE_URI:http://localhost:8081}")
 public interface UserServiceProxy {
 
-	@GetMapping("/user/{id}")
+	//@GetMapping("/user/{id}")
 	public User retrieveUser(@PathVariable("id") int id);
 	
 }
